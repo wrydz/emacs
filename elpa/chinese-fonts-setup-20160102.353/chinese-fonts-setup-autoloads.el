da0c25ba95db8d4725435abb15d7b5d00253719f
wrydz <wrydzfiying@163.com>
2018-05-10 17:22:19 +0800

    delete some files

--- elpa/chinese-fonts-setup-20160102.353/chinese-fonts-setup-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; chinese-fonts-setup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("chinese-fonts-setup.el") (22284 43813
-;;;;;;  181110 524000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; chinese-fonts-setup-autoloads.el ends here
--- elpa/dash-20160306.1222/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22269 6008 213222 978000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
